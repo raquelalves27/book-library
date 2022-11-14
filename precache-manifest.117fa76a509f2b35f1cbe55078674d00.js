0d6cfc712f4e530d5629b5c83c9a2d4c58b61457
raquelalves27 <raquelsoares304@gmail.com>
2022-11-14 17:24:13 -0300

    Updates

---
+++ precache-manifest.117fa76a509f2b35f1cbe55078674d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00129a15a9a32caf4710",
+    "url": "/raquelalves27/book-library/static/js/runtime~main.00129a15.js"
+  },
+  {
+    "revision": "3008d1b6aeaa4f860977",
+    "url": "/raquelalves27/book-library/static/js/main.3008d1b6.chunk.js"
+  },
+  {
+    "revision": "3403a2f4950ca8beeef7",
+    "url": "/raquelalves27/book-library/static/js/1.3403a2f4.chunk.js"
+  },
+  {
+    "revision": "3008d1b6aeaa4f860977",
+    "url": "/raquelalves27/book-library/static/css/main.a97a8cdf.chunk.css"
+  },
+  {
+    "revision": "b908f8d23ecd9c36ac533c65d69f20a4",
+    "url": "/raquelalves27/book-library/index.html"
+  }
+];
